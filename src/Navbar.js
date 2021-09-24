6fcf5f939adbdd639915630738e7f0dabe1cb639
seriko87 <58387052+seriko87@users.noreply.github.com>
2021-09-24 01:05:29 -0400

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,65 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <header>navbar</header>;
+  const [nav, setNav] = useState(false);
+  const refNav = useRef(null);
+  const refLinks = useRef(null);
+
+  const handleBars = () => {
+    if (nav) {
+      setNav(false);
+    } else {
+      setNav(true);
+    }
+  };
+
+  useEffect(() => {
+    const linksHeight = refLinks.current.getBoundingClientRect().height;
+    if (nav) {
+      refNav.current.style.height = `${linksHeight}px`;
+    } else {
+      refNav.current.style.height = `0px`;
+    }
+  }, [nav]);
+  return (
+    <header>
+      <div className='logo-bar'>
+        <img src={logo} alt='Coding Addict' />
+        <button className='bars' onClick={handleBars}>
+          <FaBars />
+        </button>
+      </div>
+
+      <div className='nav' ref={refNav}>
+        <ul ref={refLinks} className='ul_list'>
+          {links.map((item) => {
+            return (
+              <li key={item.id}>
+                <a href={item.url} className='nav_text'>
+                  {item.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className='social'>
+        {social.map((item) => {
+          return (
+            <a
+              href={item.url}
+              key={item.id}
+              className='social_icon'
+              target='_blank'
+              rel='noreferrer'
+            >
+              {item.icon}
+            </a>
+          );
+        })}
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
